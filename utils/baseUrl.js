b849775927af5a1fa8c8785a8f8e6f79164962a1
darkillusior <mysteryuniverse12@gmail.com>
2023-04-29 21:30:17 +0530

    hh

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:3000"
-   :"https://projectbid-4zl9mrwgx-darkillusior.vercel.app/";
+   :"http://projectbid-seven.vercel.app";
 
 module.exports = baseUrl;
