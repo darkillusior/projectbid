7fea0eaff650e24512ab9660fedad690f6719cec
KamalSinghKhanna <kamalsinghkhanna2706@gmail.com>
2023-05-13 20:01:41 +0530

    changed url

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:3000"
-   :"http://projectbid-seven.vercel.app";
+   :"https://projectbid-kamalsinghkhanna.onrender.com";
 
 module.exports = baseUrl;
