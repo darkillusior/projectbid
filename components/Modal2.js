c2607a9d02602362b12effc5ff876a8a89afc57a
KamalSinghKhanna <kamalsinghkhanna2706@gmail.com>
2023-05-24 11:25:50 +0530

    fixed ui for update info in profile page

--- components/Modal2.js
+++ components/Modal2.js
@@ -2,7 +2,6 @@ import React, { useState, useRef } from "react";
 import uploadPic from "../utils/uplodesinglefile";
 import { profileUpdate } from "@/utils/profileActions";
 export default function Modal2({}) {
-  
   const [showModal, setShowModal] = React.useState(false);
   const [media, setMedia] = useState(null);
   const [mediaPreview, setMediaPreview] = useState(null);
@@ -16,26 +15,23 @@ export default function Modal2({}) {
         return setMediaPreview(URL.createObjectURL(files[0]));
       }
     }
-    
-
   };
   const inputRef = useRef();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-   
+  const handleSubmit = async () => {
+    // e.preventDefault();
+
     let picUrl;
 
     if (media !== null) {
       picUrl = await uploadPic(media);
-      }
-     
- 
-await profileUpdate(picUrl,info)
+    }
 
-setMedia(null);
+    await profileUpdate(picUrl, info);
+
+    setMedia(null);
     mediaPreview && URL.revokeObjectURL(mediaPreview);
-}
+  };
 
   return (
     <>
@@ -44,67 +40,92 @@ setMedia(null);
         type="button"
         onClick={() => setShowModal(true)}
       >
-   Update Info
+        Update Info
       </button>
       {showModal ? (
         <>
-          <div
-            className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
-          >
-            <div className="relative h-screen my-6 mx-auto w-full">
-         
-              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full h-full bg-white outline-none focus:outline-none">
-             
-           <form onSubmit={handleSubmit}>
-           {media === null && mediaPreview === null ? (
-            <>
-              <input
-                ref={inputRef}
-                onChange={handleChange}
-                name="media"
-                style={{ display: "none" }}
-                type="file"
-              />
-              <div
-                className="flex flex-col p-5 "
-                onClick={() => inputRef.current.click()}
-              >
-                <h1 className="text-9xl" >
-                    +</h1>
-                <h1 className="font-semibold text-lg">Image</h1>
-              </div>
-            </>
-          ) : media != null && mediaPreview ? (
-            <div className="flex flex-col">
-              <img
-                style={{ height: "150px", width: "150px" }}
-                src={mediaPreview}
-                alt="PostImage"
-                centered
-                size="medium"
-              />
-              <h1 className="font-semibold text-lg">Image</h1>
-            </div>
-          ) : null}
-          <textarea 
-            placeholder="Add discription"
-            name="discription"
-            value={info}
-            onChange={(e)=>setinfo(e.target.value)}/>
+          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+            <div className="relative w-full my-6 mx-auto max-w-3xl">
+              {/*content*/}
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full card-profile outline-none focus:outline-none">
+                {/*header*/}
+                <div className="flex items-start justify-center p-5 border-b border-solid border-slate-200 rounded-t">
+                  <h3 className="text-3xl text-white font-semibold">
+                    Update Info
+                  </h3>
+                </div>
+                {/*body*/}
+                <div className="relative p-6 flex-auto">
+                  <div className="flex items-center justify-around">
+                    {media === null && mediaPreview === null ? (
+                      <>
+                        <input
+                          ref={inputRef}
+                          onChange={handleChange}
+                          name="media"
+                          style={{ display: "none" }}
+                          type="file"
+                          required
+                        />
+                        <div
+                          className="flex flex-col p-5 item-center justify-center"
+                          onClick={() => inputRef.current.click()}
+                        >
+                          <h1 className="text-8xl text-white">+</h1>
+                          <h1 className="font-semibold text-center text-lg text-white">
+                            Image
+                          </h1>
+                        </div>
+                      </>
+                    ) : media != null && mediaPreview ? (
+                      <div className="flex flex-col">
+                        <img
+                          className="rounded-xl"
+                          style={{ height: "100px", width: "100px" }}
+                          src={mediaPreview}
+                          alt="PostImage"
+                          centered
+                          size="medium"
+                        />
+                        {/* <h1 className="font-semibold text-lg">Image</h1> */}
+                      </div>
+                    ) : null}
+                    <textarea
+                      className="h-24 w-96 border-2 bg-transparent placeholder:text-white outline-none border-white border-solid rounded-lg p-2 text-white"
+                      placeholder="Add discription"
+                      name="discription"
+                      value={info}
+                      onChange={(e) => setinfo(e.target.value)}
+                      required
+                    />
 
-          <button>Update</button>
-          
-       </form> 
-          
+                    {/* <button>Update</button> */}
+                  </div>
+                </div>
+                {/*footer*/}
                 <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
                   <button
-                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    className="text-red-400 hover:text-red-600 background-transparent font-bold px-6 py-2 text-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                     type="button"
-                    onClick={() => setShowModal(false)}
+                    onClick={() => {
+                      setShowModal(false);
+                      setinfo("");
+                      setMedia(null);
+                      setMediaPreview(null);
+                    }}
                   >
                     Close
                   </button>
-                 
+                  <button
+                    className="bg-pink-500 text-white active:bg-pink-800 font-bold text-md px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => {
+                      handleSubmit();
+                      setShowModal(false);
+                    }}
+                  >
+                    Save Changes
+                  </button>
                 </div>
               </div>
             </div>
--- pages/my-profile.js
+++ pages/my-profile.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import Navbar from '../components/Navbar'
+import Navbar from "../components/Navbar";
 import axios from "axios";
 import baseUrl from "../utils/baseUrl";
 import Link from "next/link";
@@ -7,139 +7,154 @@ import { parseCookies } from "nookies";
 import Bidslist from "@/components/Bidslist";
 import Card from "@/components/Card";
 import Modal2 from "@/components/Modal2";
-function Profile  ({user,postsData}) {
+function Profile({ user, postsData }) {
+  const [post, setPost] = useState(postsData || []);
 
-  const [post, setPost] = useState( postsData||[]);
-  
-  const [Mybid, setshowMybid] = useState( false);
+  const [Mybid, setshowMybid] = useState(false);
   const [Myidea, setshowMyidea] = useState(false);
-  const [MyPost, setshowMyPost] = useState( false);
-  const [bid, setMybids] = useState( user.mybid||[]);
+  const [MyPost, setshowMyPost] = useState(false);
+  const [bid, setMybids] = useState(user.mybid || []);
 
-  const [idea, setMyidea] = useState(user.myidea||[]);
+  const [idea, setMyidea] = useState(user.myidea || []);
+
+  console.log(user)
   return (
     <>
-    
-
-    {user&&<main className="profile-page">
-  <Navbar user={user} />
-  {/* <section className="relative block h-500-px">
+      {user && (
+        <main className="profile-page">
+          <Navbar user={user} />
+          {/* <section className="relative block h-500-px">
     <div className="top-auto bottom-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden h-70-px" style={{ transform: "translateZ(0px)"}}>
       <svg className="absolute bottom-0 overflow-hidden" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none" version="1.1" viewBox="0 0 2560 100" x="0" y="0">
         <polygon className="text-blueGray-200 fill-current" points="2560 0 2560 100 0 100"></polygon>
       </svg>
     </div>
   </section> */}
-  <section className="relative bg-blueGray-200">
-    <div className="container-m px-4">
-      <div className="flex flex-col min-w-0 break-words bg-transparent w-full mb-6  rounded-lg ">
-        <div className="px-6">
-          <div className="flex flex-wrap  items-center justify-center">
-            <div className="w-1/6 lg:w-1/6  lg:order-2  flex flex-col justify-center relative -mt-16">
-             
-                <img alt="..." src={user.userimg} className="shadow-xl rounded-full  border-none w-52 h-52" />
-               <Modal2/>
-            </div>
-            <div className="w-full lg:w-4/12 px-4 flex items-center justify-center mt-5 sm:mt-0 lg:mt-0 lg:order-3 lg:text-right lg:self-center">
-              <div className="py-6 px-3 mt-32 sm:mt-0 !important">
-                <button onClick={()=>{
-                  setshowMybid(true)
-                  setshowMyidea(false)
-                  setshowMyPost(false)
-                }} className="bg-pink-500 active:bg-pink-600 uppercase text-blue-200 font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150" type="button">
-                MyBid
-                </button>
-                <button onClick={()=>{
-                  setshowMybid(false)
-                  setshowMyidea(true)
-                  setshowMyPost(false)
-                }} className="bg-pink-500 active:bg-pink-600 uppercase text-blue-200 font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150" type="button">
-                 MyIdea
-                </button>
-                <button onClick={()=>{
-                  setshowMybid(false)
-                  setshowMyidea(false)
-                  setshowMyPost(true)
-                }} className="bg-pink-500 active:bg-pink-600 uppercase text-blue-200 font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150" type="button">
-                 MyPost
-                </button>
+          <section className="relative bg-blueGray-200">
+            <div className="container-m px-4">
+              <div className="flex flex-col min-w-0 break-words bg-transparent w-full mb-6  rounded-lg ">
+                <div className="px-6">
+                  <div className="flex flex-wrap  items-center justify-center">
+                    <div className="w-3/1 lg:w-1/6  lg:order-2  flex flex-col justify-center relative -mt-16">
+                      <img
+                        alt="..."
+                        src={user.userimg}
+                        className="shadow-xl rounded-full  border-none w-52 h-52"
+                      />
+                      <Modal2 />
+                    </div>
+                    <div className="w-full lg:w-4/12 px-4 flex items-center justify-center mt-5 sm:mt-0 lg:mt-0 lg:order-3 lg:text-right lg:self-center">
+                      <div className="py-6 px-3 mt-2 sm:mt-0 !important">
+                        <button
+                          onClick={() => {
+                            setshowMybid(true);
+                            setshowMyidea(false);
+                            setshowMyPost(false);
+                          }}
+                          className="bg-pink-500 active:bg-pink-600 uppercase text-blue-200 font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none mr-2 mb-1 ease-linear transition-all duration-150"
+                          type="button"
+                        >
+                          MyBid
+                        </button>
+                        <button
+                          onClick={() => {
+                            setshowMybid(false);
+                            setshowMyidea(true);
+                            setshowMyPost(false);
+                          }}
+                          className="bg-pink-500 active:bg-pink-600 uppercase text-blue-200 font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none mr-2 mb-1 ease-linear transition-all duration-150"
+                          type="button"
+                        >
+                          MyIdea
+                        </button>
+                        <button
+                          onClick={() => {
+                            setshowMybid(false);
+                            setshowMyidea(false);
+                            setshowMyPost(true);
+                          }}
+                          className="bg-pink-500 active:bg-pink-600 uppercase text-blue-200 font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150"
+                          type="button"
+                        >
+                          MyPost
+                        </button>
+                      </div>
+                    </div>
+                    <div className="w-full lg:w-4/12 px-4 lg:order-1">
+                      <div className="flex justify-center py-4 lg:pt-4 pt-8">
+                        <div className="mr-4 p-3 text-center">
+                          <span className="text-xl font-bold block uppercase tracking-wide text-blue-200">
+                            22
+                          </span>
+                          <span className="text-sm text-blue-200">Friends</span>
+                        </div>
+                        <div className="mr-4 p-3 text-center">
+                          <span className="text-xl font-bold block uppercase tracking-wide text-blue-200">
+                            10
+                          </span>
+                          <span className="text-sm text-blue-200">Project</span>
+                        </div>
+                        <div className="lg:mr-4 p-3 text-center">
+                          <span className="text-xl font-bold block uppercase tracking-wide text-blue-200">
+                            89
+                          </span>
+                          <span className="text-sm text-blue-200">Review</span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="text-center mt-8">
+                    <h3 className="text-4xl font-semibold leading-normal  text-blue-200 mb-2">
+                      {user.name}
+                    </h3>
+                    <div className="mb-2 text-blue-200">
+                      <i className="fas fa-university mr-2 text-lg font-semi-bold text-blue-200">{user.master}</i>
+                     
+                    </div>
+                  </div>
+                  <div className="mt-10 py-10 border-t border-blueGray-200 text-center">
+                    <div className="flex flex-wrap justify-center">
+                      <div className="w-full lg:w-9/12 px-4">
+                        <Bidslist
+                          bid={Mybid ? bid : null}
+                          setMybids={setMybids}
+                          setMyidea={setMyidea}
+                          idea={Myidea ? idea : null}
+                        />
+                        {MyPost && (
+                          <>
+                            {post.map((item, index) => (
+                              <Link
+                                className="w-auto"
+                                href={`/posts/${item._id}`}
+                              >
+                                <Card key={index} post={item} />
+                              </Link>
+                            ))}
+                          </>
+                        )}
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
-            <div className="w-full lg:w-4/12 px-4 lg:order-1">
-              <div className="flex justify-center py-4 lg:pt-4 pt-8">
-                <div className="mr-4 p-3 text-center">
-                  <span className="text-xl font-bold block uppercase tracking-wide text-blue-200">22</span><span className="text-sm text-blue-200">Friends</span>
-                </div>
-                <div className="mr-4 p-3 text-center">
-                  <span className="text-xl font-bold block uppercase tracking-wide text-blue-200">10</span><span className="text-sm text-blue-200">Project</span>
-                </div>
-                <div className="lg:mr-4 p-3 text-center">
-                  <span className="text-xl font-bold block uppercase tracking-wide text-blue-200">89</span><span className="text-sm text-blue-200">Review</span>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="text-center mt-12">
-            <h3 className="text-4xl font-semibold leading-normal  text-blue-200 mb-2">
-              {user.name}
-            </h3>
-            <div className="text-sm leading-normal mt-0 mb-2 text-blue-200 font-bold uppercase">
-              <i className="fas fa-map-marker-alt mr-2 text-lg text-blue-200"></i>
-              Los Angeles, California
-            </div>
-            <div className="mb-2 text-blue-200 mt-10">
-              <i className="fas fa-briefcase mr-2 text-lg text-blue-200"></i>Solution Manager - Creative Tim Officer
-            </div>
-            <div className="mb-2 text-blue-200">
-              <i className="fas fa-university mr-2 text-lg text-blue-200"></i>University of Computer Science
-            </div>
-          </div>
-          <div className="mt-10 py-10 border-t border-blueGray-200 text-center">
-            <div className="flex flex-wrap justify-center">
-              <div className="w-full lg:w-9/12 px-4">
-          <Bidslist  
-          bid={Mybid?bid:null}
-          setMybids={setMybids}
-          setMyidea={setMyidea}
-          idea={Myidea?idea:null}
-  
-
-          
-          />
-        {MyPost&& <>{post.map((item,index)=>(    
-     <Link className="w-auto" href={`/posts/${item._id}`} >
-  
-        <Card  key={index}  post={item} />
-    
-     </Link> 
-  ) )}</>}  
-            
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-   
-  </section>
-</main>}
-
+          </section>
+        </main>
+      )}
     </>
-  )
+  );
 }
 
-export default Profile
+export default Profile;
 
 export const getServerSideProps = async (ctx) => {
   try {
- 
-
     const { token } = parseCookies(ctx);
-    const res = await axios.get(`${baseUrl}/api/profile`,
-    {
-      headers: { Authorization: token   }
+    const res = await axios.get(`${baseUrl}/api/profile`, {
+      headers: { Authorization: token },
     });
-   
+
     return { props: { postsData: res.data } };
   } catch (error) {
     return { props: { errorLoading: true } };
