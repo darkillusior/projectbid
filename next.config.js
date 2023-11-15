f7300f8a5660238ffb5b4dc7e62d78a9a6fb00af
darkillusior <94616975+darkillusior@users.noreply.github.com>
2023-11-15 20:07:53 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,1 @@
-module.exports={
-  env:{
-    CLOUDINARY_URL:"https://api.cloudinary.com/v1_1/darkillisour/image/upload",
-    client_id:"483883766761-dugimrugcrnf7us5n80fmp1evjkmbivc.apps.googleusercontent.com",
-    CLOUDINARY_URL2:"https://api.cloudinary.com/v1_1/darkillisour/image/upload"
-  }
-};
+
